23fd1c740999498ffa50094bd5dbf2133ef044f6
eyob tariku <eyobtariku@gmail.com>
2018-06-25 00:49:30 +0300

    Fix root urlconf redirect pattern

--- config/urls.py
+++ config/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from django.views import generic
+from deft import views as deft_views
 
 
 urlpatterns = [
-    path('', generic.RedirectView.as_view(url='/abl/')),
-    path('abl/', include('deft.urls')),
+    path('', deft_views.IndexView.as_view()),
     path('admin/', admin.site.urls),
 ]
